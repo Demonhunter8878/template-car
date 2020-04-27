8739b2c94e44a893c97d68484c155c23d167cc7f
Demonhunter8878 <cooldudegabe16@gmail.com>
2020-04-26 22:42:17 -0400

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -6,9 +6,6 @@ files {
 	'data/carvariations.meta',
 	'data/handling.meta',
 	'data/vehicles.meta',
-    --'data/contentunlocks.meta',
-    --'data/vehiclelayouts.meta',
-    --'data/shop_vehicle.meta',
 }
 
 
@@ -17,9 +14,5 @@ data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
 data_file 'HANDLING_FILE' 'data/handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
 
---data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
---data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
---data_file 'VEHICLE_SHOP_DLC_FILE' 'data/shop_vehicle.meta'
 
-
---client_script { 'client/vehicle_name.lua' }
+---client_script { 'client/vehicle_name.lua' }
